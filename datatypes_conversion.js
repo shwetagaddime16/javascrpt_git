23c71044a11a6dac8218944d15a5bef50b12a0a4
shweta <shwetagaddime2003@gmail.com>
2025-03-20 16:59:05 +0530

    conversion of datatypes

---
+++ datatypes_conversion.js
@@ -0,0 +1,41 @@
+let score="33abc"
+
+
+console.log(typeof score);
+//***************************************** */
+let scoree=33
+
+
+console.log(typeof scoree);
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+ score=true;
+ 
+console.log(typeof score);
+//"33" => 33
+//"33abc" => NaN
+//"true" => 1;
+//false => 0
+let isLoggedin=1
+
+let isLoggedIn = "true"; 
+let booleanISLoggedIn = Boolean(isLoggedIn); 
+console.log(booleanISLoggedIn); 
+
+isLoggedin=" "
+ isLoggedIn = "true"; 
+ booleanISLoggedIn = Boolean(isLoggedIn); 
+console.log(booleanISLoggedIn);
+
+//1==true ; 0=false
+// "" => false
+//"hitesh" =>true
+
+let someNumber= 33
+let StringNumber=String(someNumber)
+console.log(StringNumber);
+
+console.log(typeof StringNumber);
