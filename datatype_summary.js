a933005b8b09d130b7e528c8a23d84e899484967
shweta <shwetagaddime2003@gmail.com>
2025-03-25 12:23:26 +0530

    updated

---
+++ datatype_summary.js
@@ -0,0 +1,19 @@
+//const bigNumber=3456543576654356754n
+
+//reference(Nonprimitive)
+
+//Array,Objects,Functions
+
+const heros=["shaktiman","nagraj","doga"]
+let myObj={
+    name:"shweta",
+    age: 22.
+}
+const myFunction=funtion()
+{
+    console.log("hello world");
+    
+}
+
+console.log(typeof bigNumber);
+
--- datatypes.js
+++ datatypes.js
@@ -2,38 +2,38 @@
 
 //alert(3+#)   //we are using node js,not browser
 
-console.log(3+3)  //code readability shoud  be high 
-let ename ="histesh"
-let age=40
-let isLoggedin=true
-let state;
+// console.log(3+3)  //code readability shoud  be high 
+// let ename ="histesh"
+// let age=40
+// let isLoggedin=true
+// let state;
 
-//number  
-//bigint
-// String=""
-// Boolean= true or false 
-//null = standalone value
-//undefined = 
+// //number  
+// //bigint
+// // String=""
+// // Boolean= true or false 
+// //null = standalone value
+// //undefined = 
 
-let fullName="shweta star";
-age=24;
-price =1000;
-console.log(fullName)
+// let fullName="shweta star";
+// age=24;
+// price =1000;
+// console.log(fullName)
 
-let a;
-console.log(a);
+// let a;
+// console.log(a);
 
 
-{
-    let a=5;
-    //let a=7; // here redefine is **not posible** in one block
-    console.log(a);
-}
-{
-    let a=7;
-    // here redefine is **posible** in one block
-    console.log(a);
-}
+// {
+//     let a=5;
+//     //let a=7; // here redefine is **not posible** in one block
+//     console.log(a);
+// }
+// {
+//     let a=7;
+//     // here redefine is **posible** in one block
+//     console.log(a);
+// }
 
 
 
@@ -142,24 +142,24 @@ for(let key in student){
 //print all even number 1 to 11
 
 
-for(let num=0;num<=100; num++)
-{
-if(num %2 !==0)
-{
-    console.log("num=",num);
-}
+// for(let num=0;num<=100; num++)
+// {
+// if(num %2 !==0)
+// {
+//     console.log("num=",num);
+// }
 
-} 
+// } 
 
 
 
-let gameNum =25;
-let userNum = prompt("guess the game number:");
+// let gameNum =25;
+// let userNum = prompt("guess the game number:");
 
-while(userNum !=gameNum){
-  userNum = promt("you entered wrong no");
-}
-console.log("congrats!,you enter the right number");
+// while(userNum !=gameNum){
+//   userNum = promt("you entered wrong no");
+// }
+// console.log("congrats!,you enter the right number");
 
 
 // var a=10;
@@ -176,6 +176,82 @@ let symbol1=Symbol("hello");
 let symbol2=Symbol("hello");
 console.log(symbol1==symbol2);
 console.log();
+
+//#primitive datatypes
+
+
+//7 types :
+// String ,Number,Boolean,null,undefined,Symbol,BigInt 
+
+const score = 100 
+const scoreValue=100.3
+const isLoggedIn=false
+const outsideTemp=null
+
+let userEmail=undefined;
+let email;
+
+const id=Symbol('123')
+const anotherId= Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber= 34543576654356754n
+
+
+//reference type is called (nonprimitive )  and its returnn type is object
+//arrays,objects,funtions
+
+
+const heros=["shaktiman","nagraj","nagachaitanya"];
+
+
+let myObject = {
+    name:"ram",
+    age:22
+
+}
+
+// function
+
+ const myFunction=function()
+{
+  console.log("hello world");
+
+  
+}
+r
+//https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// typeOF(variable type)       Result
+
+// Undefined                   undefined
+// Boolean                     Boolean
+// null                        Object
+// number                      number
+// String                     String
+// object                     object function
+
+
+    //      Symbol()	                                           Symbol.for()
+    // प्रत्येक वेळेस नवीन Symbol तयार करतो	             ग्लोबल रजिस्टर मध्ये Symbol शोधतो
+    // समान नाव असले तरी वेगळे Symbol असतात	         समान नाव वापरल्यास आधीचा Symbol परत मिळतो
+    // Symbol.keyFor() वापरू शकत नाही	                Symbol.keyFor() ने त्याचे नाव मिळू शकते
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
